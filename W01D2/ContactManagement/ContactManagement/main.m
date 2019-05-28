9d3085d67c286ad1e987a4daf76288198bf38b7e
absdrenaline <asoni@dev>
2019-05-28 16:11:02 -0700

    Contacts - Initial commit

---
+++ W01D2/ContactManagement/ContactManagement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContactManagement
+//
+//  Created by Arun on 2019-05-28.
+//  Copyright © 2019 LogicIO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
