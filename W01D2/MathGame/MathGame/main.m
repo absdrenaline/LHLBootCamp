9d787c0f3e3ccacc722c023ca5954d081c8b8816
absdrenaline <asoni@dev>
2019-05-28 12:36:49 -0700

    MathGame - Accepting user input

--- W01D2/MathGame/MathGame/main.m
+++ W01D2/MathGame/MathGame/main.m
@@ -8,9 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+static const int kMaxInputSize = 5;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        while (YES) {
+            
+            printf("Enter a value: ");
+            
+            //Capture a number from the user;
+            char input[kMaxInputSize];
+            fgets(input,kMaxInputSize,stdin);
+            
+            NSString* inputString = [NSString stringWithCString:input encoding:NSUTF8StringEncoding];
+            
+            //Intialize a charset to clean out the user input
+            NSCharacterSet *trimCharacterSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+            NSString *cleanInputString = [inputString stringByTrimmingCharactersInSet:trimCharacterSet];
+            
+            NSLog(@"You input: %@",cleanInputString);
+            
+            
+        }
     }
     return 0;
 }
