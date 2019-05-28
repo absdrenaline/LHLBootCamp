2b176562347e0b1da00d5b37315fee58a2403bfe
absdrenaline <asoni@dev>
2019-05-28 12:21:45 -0700

    MathGame - Initial Game

---
+++ W01D2/MathGame/MathGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MathGame
+//
+//  Created by Arun on 2019-05-28.
+//  Copyright © 2019 LogicIO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
