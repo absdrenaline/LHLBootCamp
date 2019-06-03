531590f77e62196b001a6dc4bf9859715868b990
absdrenaline <asoni@dev>
2019-06-03 14:37:38 -0700

    2Player Math Game - Inital Commmit

---
+++ W02D1/MathGame/MathGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathGame
+//
+//  Created by Arun on 2019-06-03.
+//  Copyright © 2019 LogicIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W02D1/MathGame/MathGame/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MathGame
+//
+//  Created by Arun on 2019-06-03.
+//  Copyright © 2019 LogicIO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
