7780cb3966c188c4466172a5490c8f8cb575d4fa
absdrenaline <asoni@dev>
2019-06-03 19:28:40 -0700

    2Player Math Game - Complete with Stretch + Cleanup

--- W02D1/MathGame/MathGame/ViewController.m
+++ W02D1/MathGame/MathGame/ViewController.m
@@ -28,12 +28,12 @@
     score1.text = [NSString stringWithFormat:@"%@: %lu",[model playerName:0],(unsigned long)[model playerScore:0]];
     score2.text = [NSString stringWithFormat:@"%@: %lu",[model playerName:1],(unsigned long)[model playerScore:1]];
     inputString.text = @"";
-    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     model = [GameModel new];
+    
     [self nextTurn];
     statusString.text = @"";
 }
@@ -42,34 +42,20 @@
     
     UIButton *button = (UIButton *)sender;
  
-    switch (button.tag) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-           inputString.text = [inputString.text stringByAppendingFormat:@"%ld",button.tag];
-            break;
-        case 11:
-            if ([inputString.text hasPrefix:@"-"]) {
-                inputString.text = [inputString.text substringFromIndex:1];
-            } else {
-                inputString.text = [NSString stringWithFormat:@"-%@",inputString.text];
-            }
-            break;
-        case 12:
-            if (![inputString.text containsString:@"."]) {
-                inputString.text = [inputString.text stringByAppendingString:@"."];
-            }
-            break;
-        default:
-            break;
-            
+    if (button.tag < 10) {
+        inputString.text = [inputString.text stringByAppendingFormat:@"%ld",button.tag];
+    }
+    else if(button.tag == 11) {
+        if ([inputString.text hasPrefix:@"-"]) {
+            inputString.text = [inputString.text substringFromIndex:1];
+        } else {
+            inputString.text = [NSString stringWithFormat:@"-%@",inputString.text];
+        }
+    }
+    else if (button.tag == 12) {
+        if (![inputString.text containsString:@"."]) {
+            inputString.text = [inputString.text stringByAppendingString:@"."];
+        }
     }
 }
 
@@ -94,15 +80,10 @@
     }
     [UIView animateWithDuration:1
                            animations:^{self->statusString.alpha = 0.0;}];
-     [self nextTurn];
-//    if(model.isGameOver) {
-//
-//    } else {
-//
-//    }
-//
+    [self nextTurn];
     
 }
+
 
 -(void) doGameOver {
     puzzleString.text = [NSString stringWithFormat:@"%@ LOST",[model currentPlayer]];
@@ -110,23 +91,7 @@
     statusString.textColor = UIColor.redColor;
     statusString.alpha = 1.0;
     
-    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Game Over"
-                                                                   message:@"Would you like to play another game?"
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-    
-    UIAlertAction* yesAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault
-                                                      handler:^(UIAlertAction * action) {
-                                                          [self reset];
-                                                      }];
-    UIAlertAction* noAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault
-                                                     handler:^(UIAlertAction * action) {
-                                                         
-                                                     }];
-    
-    [alert addAction:yesAction];
-    [alert addAction:noAction];
-    
-    [self presentViewController:alert animated:YES completion:nil];
+    [self showResetDialog];
     
 }
 
@@ -134,6 +99,33 @@
     model = [GameModel new];
     [self nextTurn];
     statusString.text = @"";
+}
+
+- (void)showResetDialog {
+    UIAlertController* alert = [UIAlertController
+                                alertControllerWithTitle:@"Game Over"
+                                message:@"Would you like to play another game?"
+                                preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* yesAction = [UIAlertAction
+                                actionWithTitle:@"Yes"
+                                style:UIAlertActionStyleDefault
+                                handler:^(UIAlertAction * action) {
+                                    [self reset];
+                                }
+                                ];
+    
+    UIAlertAction* noAction = [UIAlertAction
+                               actionWithTitle:@"No"
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction * action) {
+                               }
+                               ];
+    
+    [alert addAction:yesAction];
+    [alert addAction:noAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
