7b40ebd8bd4e34857664bf0d40279369d8a910b6
absdrenaline <asoni@dev>
2019-06-15 19:48:48 -0700

    Step 1: Setting up the map 0 complete

--- W03D5/UberBean/UberBean/ViewController.m
+++ W03D5/UberBean/UberBean/ViewController.m
@@ -2,19 +2,38 @@
 // Created: 2019-06-14
 //
 
-
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    CLLocationManager* locationManager;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager requestWhenInUseAuthorization];
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    CLLocation *lastLocation = [locations lastObject];
+    self.mapView.region = MKCoordinateRegionMake(lastLocation.coordinate, MKCoordinateSpanMake(0.03, 0.03));
+    
+}
 
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if(status == kCLAuthorizationStatusAuthorizedAlways || status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [locationManager requestLocation];
+    }
+}
 @end
