0f93fdaf9a0ace608821c0213de16dc0445e966b
absdrenaline <asoni@dev>
2019-07-08 17:27:08 -0700

    Food Tracker Example App

---
+++ W06DE/Food Tracker/Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,25 @@
+////  MealTableViewCell.swift
+// Created: 2019-07-08
+//
+
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
